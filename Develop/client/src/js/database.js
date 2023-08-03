f07b978408fe31bbf01042bae7a4a25e5a2610e9
Zero_X <rvkov536@gmail.com>
2023-08-03 00:43:47 -0400

    updated database js with logic for both database methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+
+    // Add the content to the database
+    await store.add(content);
+
+    console.log('Content added to the database:', content);
+  } catch (error) {
+    console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
 
+    // Retrieve all content from the database
+    const content = await store.getAll();
+
+    console.log('Content retrieved from the database:', content);
+    return content;
+  } catch (error) {
+    console.error('getDb not implemented');
+    return [];
+  }
+}
 initdb();
